756bbf09cc8b4bd4e9219940f9a789feea04eaba
veekshith gandhi <veekshithgandhi@gmail.com>
2021-10-28 13:14:09 +0530

    login completed

---
+++ src/auth/login.js
@@ -0,0 +1,22 @@
+function handleLoginBtn() {
+  const loginBtn = document.querySelector(".login-btn-signup");
+  const email = document.querySelector(".login-email");
+  const password = document.querySelector(".login-password");
+  loginBtn.addEventListener("click", function () {
+    handleLogindetails(email.value, password.value);
+  });
+}
+
+function handleLogindetails(email, password) {
+  return fetch("http://localhost:8000/users")
+    .then((res) => res.json())
+    .then((res) => {
+      res.forEach((element) => {
+        if (email == element.email && password == element.password) {
+          window.location.href = "./signup.html";
+        }
+      });
+    });
+}
+
+window.addEventListener("load", handleLoginBtn);
